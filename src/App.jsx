421917e20138ebe0bd7d5b9477f17c4dc039f62b
shivanshsingh <12shivanshsingha@gmail.com>
2025-06-11 10:58:29 +0530

    added converting feature with suitable useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,56 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Export from './components/Export'
 import InputBox from './components/InputBox'
+import useCurrency from './hooks/useCurrency.js'
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [from,setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [amount,setAmount]=useState(0);
+  const [convertedAmount,setConvertedAmount]=useState(0);
+  const rates = useCurrency(from);
+  const items=Object.keys(rates);
+  console.log(items);
 
+  // swap functionality
+  const swap = () =>{
+    setAmount(convertedAmount);
+    setConvertedAmount(amount);
+    setTo(from);
+    setFrom(to);
+  }
+  // convert functionality
+  
+    const convert = useEffect(()=>{
+      setConvertedAmount(amount*rates[to]);
+    },[amount,swap]); // this will give the user a real time effect of calculating the change as each time the amount is changed it triggers the recalculcultion and when swap function is called then also
+
+  // this is the power of useEffect as it helps to automatically do the things
+  
   return (
     <>
        <div className='bg-black h-screen w-full text-white rounded p-2 flex flex-col items-center justify-center space-y-4 '>
         <h1>CURRENCY CONVERTER</h1>
-          <InputBox/>
-          <button className='bg-blue-400 p-2 m-3 rounded-3xl w-40 hover:bg-blue-600 cursor-pointer active:bg-blue-800'>Swap</button>
-          <InputBox/>
+          <InputBox
+          items={items}
+          amount={amount}
+          currency={from}
+          onCurrencyChange={setFrom}
+          onAmountChange={setAmount}
+          />
+          <button onClick={swap} className='bg-blue-400 p-2 m-3 rounded-3xl w-40 hover:bg-blue-600 cursor-pointer active:bg-blue-800'>Swap</button>
+          <InputBox
+            items={items}
+            amount={convertedAmount}
+            currency={to}
+            onAmountChange={setConvertedAmount}
+            onCurrencyChange={setTo}
+            change={true}
+          />
+          <button onClick={convert} className='bg-green-400 p-2 m-3 rounded-3xl w-40 hover:bg-green-600 cursor-pointer active:bg-green-800'>Convert {from} to {to}</button>
        </div>
     </>
   )
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 
-function InputBox() {
+function InputBox({items=["usd"], label="", amount, currency="usd", change, onAmountChange, onCurrencyChange}) {
   return (
     <div className="bg-stone-800 rounded-xl p-4 m-4 w-full max-w-md shadow-lg">
       <div className="flex flex-col sm:flex-row items-center gap-4">
+        <h1>{label}</h1>
         <input
           className="bg-orange-300 text-black text-center rounded-full p-2 w-full focus:outline-none focus:ring-2 focus:ring-orange-500 transition"
           type="number"
           placeholder="Enter amount"
+          value={amount}
+          readOnly={change} // hangeable or not 
+          onChange={(e) => onAmountChange && onAmountChange(e.target.value)} // what happens when anything changes inside of it
         />
-        <select className="bg-white text-black rounded-full p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 transition">
-          <option value="usd">USD</option>
-          <option value="inr">INR</option>
-          <option value="eur">EUR</option>
+        <select className="bg-white text-black rounded-full p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+        value={currency}
+        onChange={(e)=> onCurrencyChange && onCurrencyChange(e.target.value)}
+        >
+          {
+            items.map((cur)=>(
+                <option key={cur} value={cur}>{cur}</option>
+            ))
+          }
         </select>
       </div>
     </div>
